231f42dd3792e865a49e0e99af3426fdc44f2d03
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:36:24 +0800

    PageLink.jsx

--- src/componants/PageLink.jsx
+++ src/componants/PageLink.jsx
@@ -1,18 +1,8 @@
-import React from 'react'
-
-function PageLink( { groupName, itemName }) {
+const PageLink = ({ itemName, href, text }) => {
   return (
-   <ul className={groupName}>
     <li className={itemName}>
-        <a href="index.html">Home</a></li>
-    <li className={itemName}>
-        <a href="#about">About</a></li>
-    <li className={itemName}>
-        <a href="#services">Services</a></li>
-    <li className={itemName}>
-        <a href="#tours">Tours</a></li>
-    </ul>
-  )
-}
+        <a href={href}>{text}</a></li>
+  );
+};
 
 export default PageLink
