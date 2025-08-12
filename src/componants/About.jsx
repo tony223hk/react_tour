2e4b7dce43814b52fd651e47d9a9148264945cce
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:37:17 +0800

    About.jsx

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import Title from "./Title";
+import img from "../assets/background.jpg";
 
 const About = () => {
   return (
    <section className="about" id="about">
-      <h2>about <span className="text-secondary">us</span></h2>
+      <Title title='about' subtitle='us'/>
       <div className="about-content">
         <div className="about-img">
-          <img src="./img/background.jpg" alt="about" />
+          <img src={img} alt="about" />
         </div>
         <div className="text">
           <h3>expolore the difference</h3>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
-          <a href="#" className="btn-about">read more</a>
+          <a href="#home" className="btn-about">read more</a>
         </div>
       </div>
     </section>
