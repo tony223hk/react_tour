235ad7a9074d7a07eed16b07f80772d821a3f0e9
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:34:46 +0800

    Tours.jsx

--- src/componants/Tours.jsx
+++ src/componants/Tours.jsx
@@ -1,74 +1,22 @@
 import React from 'react'
+import { tours } from "../data";
+import Tour from "./Tour";
+import Title from "./Title";
 
 const Tours = () => {
   return (
     <article className="tours" id="tours">
-      <h2>featured <span className="text-secondary">tours</span></h2>
+      <Title title='featured' subtitle='tours'/>
       <div className="cards">
-        {/*<!-- card X 4 -->*/}
-        <div className="card">
-          <div className="card-img">
-            <img src="./img/tour1.jpeg" alt="" />
-            <p>Aug 1st 2025</p>
-          </div>
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour} />
+        ))}
+        </div>
+    </article>
+    );
+  };
 
-          <div className="card-content">
-            <h3>adventure</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-              <li><i className="fa-solid fa-map"></i>China</li>
-              <li>6 Days</li>
-              <li>From $2100</li>
-            </ul>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="./img/tour2.jpeg" alt="" />
-            <p>Aug 1st 2025</p>
-          </div>
-
-          <div className="card-content">
-            <h3>adventure</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-              <li><i className="fa-solid fa-map"></i>China</li>
-              <li>6 Days</li>
-              <li>From $2100</li>
-            </ul>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="./img/tour3.jpeg" alt="" />
-            <p>Aug 1st 2025</p>
-          </div>
-          <div className="card-content">
-            <h3>adventure</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-              <li><i className="fa-solid fa-map"></i>China</li>
-              <li>6 Days</li>
-              <li>From $2100</li>
-            </ul>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="./img/tour4.jpeg" alt="" />
-            <p>Aug 1st 2025</p>
-          </div>
-          <div className="card-content">
-            <h3>adventure</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-              <li><i className="fa-solid fa-map"></i>China</li>
-              <li>6 Days</li>
-              <li>From $2100</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+export default Tours
     </article>
   )
 }
