571ff8e500abfa230250ccdefa377e43372cd397
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:37:45 +0800

    Footer.jsx

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import PageLink from './PageLink'
+import PageLinks from './PageLinks'
 import SocialLink from "./SocialLink";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-menu">
-        <PageLink groupName= "menu-list" itemName="menu-item" />
+        <PageLinks groupName= "menu-list" itemName="menu-item" />
       </div>
       <div className="footer-icon-menu">
         <SocialLink />
       </div>
-      <p>Copyright &copy; <span id="date"></span> All Rights Reserved</p>
+      <p>Copyright &copy; 
+        <span id="date">{new Date().getFullYear()}</span> All Rights Reserved</p>
     </footer>
   )
 }
