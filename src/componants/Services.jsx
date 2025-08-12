45a2b292415b399d20788580066f9713109e596a
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:42:30 +0800

    Services.jsx

--- src/componants/Services.jsx
+++ src/componants/Services.jsx
@@ -1,37 +1,18 @@
-import React from 'react'
+import Title from "./Title";
+import { services } from "../data";
+import Service from "./Service";
 
 const Services = () => {
   return (
-  <main className="services" id="services">
-      <h2>our <span className="text-secondary">services</span></h2>
-      <div className="services-content">
-        <div className="service-title">
-          <i className="fa-solid fa-wallet"></i>
-          <h3>saving money</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            assumenda.
-          </p>
-        </div>
-        <div className="service-title">
-          <i className="fa-solid fa-tree"></i>
-          <h3>endless hiking</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perspiciatis, eius.
-          </p>
-        </div>
-        <div className="service-title">
-          <i className="fa-solid fa-socks"></i>
-          <h3>amazing comfort</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            provident saepe laborum quibusdam corrupti recusandae numquam, iure
-          </p>
-        </div>
+    <main className='services' id='services'>
+      <Title title='our' subtitle='services' />
+      <div className='services-content'>
+        {services.map((service) => (
+          <Service key={service.id} {...service} />
+        ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
