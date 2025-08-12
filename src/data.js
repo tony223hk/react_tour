fc0a74de5d7f53892c604c52fd25ff162d0678dd
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:49:43 +0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,90 @@
+import tour1 from "/src/assets/tour1.jpeg";
+import tour2 from "/src/assets/tour2.jpeg";
+import tour3 from "/src/assets/tour3.jpeg";
+import tour4 from "/src/assets/tour4.jpeg";
+
+export const tours = [
+    {   
+        id: 1,
+        image: tour1,
+        date: "Aug 11th, 2025",
+        title: "Japan Adventure",
+        info: "Lorem ipsum dolor sit amet.",
+        location: "Japan",
+        duration: 6,
+        cost: 8000,
+    },
+    {   
+        id: 2,
+        image: tour2,
+        date: "Aug 12th, 2025",
+        title: "Hong Kong Adventure",
+        info: "Lorem ipsum dolor sit amet.",
+        location: "Hong Kong",
+        duration: 2,
+        cost: 800,
+    },
+    {   
+        id: 3,
+        image: tour3,
+        date: "Aug 13th, 2025",
+        title: "China Adventure",
+        info: "Lorem ipsum dolor sit amet.",
+        location: "China",
+        duration: 5,
+        cost: 2800,
+    },
+    {   
+        id: 4,
+        image: tour4,
+        date: "Aug 14th, 2025",
+        title: "France Adventure",
+        info: "Lorem ipsum dolor sit amet.",
+        location: "France",
+        duration: 6,
+        cost: 20000,
+    },
+];
+
+export const pageLinks = [
+    {id: 1, 
+        href: "index.html", 
+        text: "Home"},
+    {id: 2, 
+        href: "#about", 
+        text: "About"},
+    {id: 3, 
+        href: "#services", 
+        text: "Services"},
+    {id: 4, 
+        href: "#tours", 
+        text: "Hours"},
+];
+
+
+export const socialLinks = [
+    {id: 1, 
+        href: "http://www.facebook.com", 
+        icon: "fa-brands fa-facebook"},
+    {id: 2, 
+        href: "http://www.instrgram.com", 
+        icon: "fa-brands fa-instagram"},
+    {id: 3, 
+        href: "http://www.threads.com", 
+        icon: "fa-brands fa-threads"},
+];
+
+export const services = [
+    {id: 1, 
+        icon: "fa-solid fa-wallet", 
+        title: "saving money", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, assumenda."},
+    {id: 2, 
+        icon: "fa-solid fa-tree", 
+        title: "endless hiking", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, eius."},
+    {id: 3, 
+        icon: "fa-solid fa-socks", 
+        title: "amazing comfort", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam provident saepe laborum quibusdam corrupti recusandae numquam, iure."},
+]
