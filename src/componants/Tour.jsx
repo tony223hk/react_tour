3b431cb43e2c9138033127cfa3cd5130fc1b0f5a
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-12 12:44:31 +0800

    Add files via upload

---
+++ src/componants/Tour.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Tour = ({ image, date, title, info, location, duration, cost }) => {
+  return (
+    <div className="card">
+    <div className="card-img">
+    <img src={image} alt="" />
+    <p>{date}</p>
+    </div>
+     <div className="card-content">
+     <h3>{title}</h3>
+     <p>{info}</p>
+     <ul>
+        <li><i className="fa-solid fa-map"></i>{location}</li>
+        <li>{duration}</li>
+        <li>{cost}</li>
+        </ul>
+    </div>
+    </div>
+  );
+};
+
+export default Tour
